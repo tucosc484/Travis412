b02b835ff018275a3a6b18f05c0c989b10ecf3ed
AsuraJoe <skyrider245@gmail.com>
2023-03-05 12:25:40 -0500

    added functions to calculator

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,16 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	
+	public int minus(int a, int b) {
+		return a - b;
+	}
+	
+	public int mul(int a, int b) {
+		return a * b;
+	}
+	
+	public int div(int a, int b) {
+		return a / b;
+	}
 }
